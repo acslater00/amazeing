fb516067f48b7102b9f88ed750af3d9e0049142e
Adam Cohen <acslater@gmail.com>
2015-12-25 22:58:14 -0500

    drawing the solution

--- bitmap.py
+++ bitmap.py
@@ -2,7 +2,8 @@ from wand.image import Image
 import numpy as np
 
 pixels = []
-image = Image(filename="mazes/mazewhite.png")
+image = Image(filename="mazes/easy2.png")
+#image = Image(filename="mazes/mazewhite.png")
 width, height = image.width, image.height
 blob = image.make_blob(format='gray')
 
@@ -18,3 +19,38 @@ array = array.reshape(width, height)
 import dotgraph
 dg = dotgraph.DotDetector(array)
 graph = dg.generate_graph()
+
+import search
+
+start = (340, 33)
+end = (215, 365 )
+start_id = graph.pixel_to_node(start).id
+end_id = graph.pixel_to_node(end).id
+
+cost, path = search.min_path2(graph, start_id, end_id)
+
+
+# EASY 400X400 MAZE
+# start = (340, 33)
+# end = (215, 367)
+
+# path is a list of nodes
+
+def pixels_to_highlight(graph, path):
+    all_pixels = set()
+    for node_id in path:
+        node = graph.node(node_id)
+        pixels = set(node.pixels)
+        all_pixels |= pixels
+    return all_pixels
+
+from wand.drawing import Drawing
+from wand.color import Color
+
+pth = pixels_to_highlight(graph, path)
+with Drawing() as draw:
+    draw.fill_color=Color("red")
+    for pixel in pth:
+        draw.point(pixel[1], pixel[0])
+    draw(image)
+    image.save(filename="mazes/easy-solved.png")
